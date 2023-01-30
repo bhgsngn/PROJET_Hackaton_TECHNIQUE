19603ab286ea5ed593f864e1d5599c6f8b8f228e
Coolkid-RsteN <roman.giovannone-lean@edu.devinci.fr>
2023-01-30 16:48:09 +0100

    maj agents.py, création de warehouse.py, mission.py et fleet.py contenant les class du meme nom

---
+++ fleet.py
@@ -0,0 +1,20 @@
+import random
+import math
+
+class Fleet:
+    def init(self, agents, missions, warehouses):
+    self.agents = agents
+    self.missions = missions
+    self.warehouses = warehouses
+
+    def assign_missions(self):
+        # Fonction pour assigner des missions aux agents
+        for mission in self.missions:
+            if mission.status == "En attente":
+                for agent in self.agents:
+                    if agent.battery > 0:
+                        path = agent.move_a_star(mission.source.position)
+                        if path:
+                            agent.move_memorized(path)
+                            if mission.update_status(agent):
+                                break
---
+++ warehouse.py
@@ -0,0 +1,23 @@
+import random
+import math
+
+class Warehouse:
+    def init(self, position, items):
+    self.position = position
+    self.items = items
+    
+    def take_item(self, item_type, quantity):
+    # Fonction pour prendre un certain nombre d'articles d'un type donné
+        if item_type in self.items and self.items[item_type] >= quantity:
+            self.items[item_type] -= quantity
+            return True
+        else:
+            return False
+
+    def put_item(self, item_type, quantity):
+        # Fonction pour déposer un certain nombre d'articles d'un type donné
+        if item_type in self.items:
+            self.items[item_type] += quantity
+        else:
+            self.items[item_type] = quantity
+        return True
