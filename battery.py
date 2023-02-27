d1c8e86c2e8d9b46b9d5d2335292719909e37dcf
RainderAx <sbst1.elz@gmail.com>
2023-02-27 16:19:41 +0100

    explication

--- battery.py
+++ battery.py
@@ -1,18 +1,21 @@
 import time
-
+#création de la batterie
 class Battery:
     def __init__(self, energy):
         self.energy = min(max(0, energy), 50)
 
+    #variable qui defini l'énergie
     def get_energy_level(self):
         return self.energy
-
+   
+    #variable qui utilise la batterie
     def use_battery(self, amount):
         self.energy -= amount
         if self.energy <= 0:
             print("La batterie est épuisée.")
             self.energy = 0
-    #décroissanc
+
+    #variable qui fais diminuer la batterie en fonction du temps
     def decrease_energy_over_time(self, rate, time_elapsed):
         self.energy -= rate * time_elapsed
         if self.energy <= 0:
