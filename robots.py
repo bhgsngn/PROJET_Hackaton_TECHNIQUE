dcf10471e13d6be55071951de43af25a6581c54f
RainderAx <sbst1.elz@gmail.com>
2023-04-16 14:40:10 +0200

    CéObscurToutCa

--- robots.py
+++ robots.py
@@ -2,22 +2,22 @@ from battery import Battery
 import random
 
 class PurplePoint:
-    def __init__(self, canvas, battery):
-        self.canvas = canvas
+    def __init__(self, grid, x=0, y=0):
+        self.grid = grid
         self.battery = Battery(50)
-        self.shape = self.canvas.create_oval(10, 10, 30, 30, fill="purple")
-        self.x = random.randint(0, 400)
-        self.y = random.randint(0, 400)
+        self.shape = self.grid.canvas.create_oval(10, 10, 30, 30, fill="purple")
+        self.x = x
+        self.y = y
 
     def move(self, dx, dy):
         self.x += dx
         self.y += dy
-        self.canvas.move(self.shape, dx, dy)
+        self.grid.canvas.move(self.shape, dx*self.grid.square_size, dy*self.grid.square_size)
         self.battery.use_battery(1)
 
     def draw(self):
         self.grid.canvas.create_oval(self.x * self.grid.square_size + 2, self.y * self.grid.square_size + 2, 
-            (self.x+1) * self.grid.square_size - 2, (self.y+1) * self.grid.square_size - 2, fill=self.color)
+            (self.x+1) * self.grid.square_size - 2, (self.y+1) * self.grid.square_size - 2, fill="purple")
 
     def erase(self):
         self.grid.canvas.create_rectangle(self.x * self.grid.square_size, self.y * self.grid.square_size, 
@@ -25,26 +25,24 @@ class PurplePoint:
 
 
 class PinkPoint:
-    def __init__(self, canvas, battery):
-        self.canvas = canvas
+    def __init__(self, grid, x=15, y=15):
+        self.grid = grid
         self.battery = Battery(50)
-        self.shape = self.canvas.create_oval(10, 10, 30, 30, fill="pink")
-        self.x = random.randint(0, 400)
-        self.y = random.randint(0, 400)
+        self.shape = self.grid.canvas.create_oval(10, 10, 30, 30, fill="pink")
+        self.x = x
+        self.y = y
 
     def move(self, dx, dy):
         self.x += dx
         self.y += dy
-        self.canvas.move(self.shape, dx, dy)
+        self.grid.canvas.move(self.shape, dx*self.grid.square_size, dy*self.grid.square_size)
         self.battery.use_battery(1)
 
 
     def draw(self):
         self.grid.canvas.create_oval(self.x * self.grid.square_size + 2, self.y * self.grid.square_size + 2, 
-            (self.x+1) * self.grid.square_size - 2, (self.y+1) * self.grid.square_size - 2, fill=self.color)
+            (self.x+1) * self.grid.square_size - 2, (self.y+1) * self.grid.square_size - 2, fill="pink")
 
     def erase(self):
         self.grid.canvas.create_rectangle(self.x * self.grid.square_size, self.y * self.grid.square_size, 
             (self.x+1) * self.grid.square_size, (self.y+1) * self.grid.square_size, fill="red")
-
-
